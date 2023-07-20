48bbfc802dc2cd9bac9ac1d23a85171d889329c4
Pierre Garamendi <pierre.garamendi@gmail.com>
2023-07-19 20:30:12 -0500

    some more changes were added

--- src/scenes/main/topbar/logo/logo.tsx
+++ src/scenes/main/topbar/logo/logo.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Logo = () => {
     return (
         <div className="flex items-center cursor-pointer">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Nostromo Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Nostromo</span>
+            <Link to='/' className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white no-underline" aria-current="page">Nostromo</Link>
         </div>
     );
 }
